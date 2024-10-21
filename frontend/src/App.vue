99f43c697b84e2de217c41d80f27fda9d900590a
rcaruhel <remyca.caruhel@gmail.com>
2024-10-21 08:23:15 +0200

    AJout de l'image et de la route vers la page principale en cliquant sur l'image

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <nav>
+      <router-link to="/"
+        ><img alt="Festival logo" src="../public/logoFestivalDeCannes.png"
+      /></router-link>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
@@ -13,20 +16,11 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+img {
+  max-width: 3cm;
 }
 </style>
+<script setup lang="ts"></script>
