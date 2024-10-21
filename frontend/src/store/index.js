9375a4b44c99ee4aea0979876b69f5fd51d2c65b
rcaruhel <remyca.caruhel@gmail.com>
2024-10-21 09:44:50 +0200

    mise à jour de l'index du store pour récupéer les prestataires

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,10 +3,28 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+import PrestatairesService from "../services/prestataires.service";
+
 export default new Vuex.Store({
-  state: {},
+  state: () => ({
+    prestataires: [],
+  }),
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    updatePrestataires: (state, prestataires) => {
+      state.prestataires = prestataires;
+    },
+  },
+  actions: {
+    async getAllPrestataires({ commit }) {
+      console.log("récupération des prestataires");
+      let response = await PrestatairesService.getAllPrestataires();
+      if (response.error === 0) {
+        commit("updatePrestataires", response.data);
+      } else {
+        console.log(response.data);
+      }
+    },
+  },
   modules: {},
 });
