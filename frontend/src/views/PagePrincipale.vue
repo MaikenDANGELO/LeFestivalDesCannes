38b2ceddd9501c385fb3acf0bbcdf5cf5db968e5
Maïken D'Angelo <maiken.dangelo@gmail.com>
2024-11-11 10:57:48 +0100

    filtre multi citeres

--- frontend/src/views/PagePrincipale.vue
+++ frontend/src/views/PagePrincipale.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="hello">
     <BanniereAccueil></BanniereAccueil>
-    <div class="liste-prestataires" id="prestataires">
-      <h2>Prestataires</h2>
-      <div class="prestataires-row" v-for="row in getPrestaRows()" :key="row[0].id">
-        <div v-for="prestataire in row" :key="prestataire.id" class="prestataire-card">
-          <CartePrestatairePerso
-            :nom="prestataire.nom"
-            :descriptionAccueil="prestataire.description_accueil"
-            :image="prestataire.image"
-            :pers-page-route="`/prestataire/${prestataire.id}`"></CartePrestatairePerso>
+    <div class="listes-container">
+      <div class="filtre">
+        <label for="search">Rechercher : </label>
+        <input v-model="filtreSearch" type="search" id="search"><br>
+        <div class="filtre-check" id="filtre-type">
+          <h3>Filtre type :</h3>
+          <input type="radio" id="prestataires" value="prestataires" v-model="filtreType">
+          <label for="prestataires"> Prestataires</label><br>
+          <input type="radio" id="sponsors" value="sponsors" v-model="filtreType">
+          <label for="sponsors"> Sponsors</label>
+        </div>
+        <div class="filtre-check" id="filtre-cat" v-if="filtreType === 'prestataires'">
+          <h3>Filtre catégorie : </h3>
+          <input @click="handleCatFilter('Activité')" type="checkbox" id="activite" checked>
+          <label for="activite"> Activite</label><br>
+          <input @click="handleCatFilter('Mascotte')" type="checkbox" id="mascotte" checked>
+          <label for="mascotte"> Mascotte</label><br>
+          <input @click="handleCatFilter('Gastronomie')" type="checkbox" id="gastronomie" checked>
+          <label for="gastronomie"> Gastronomie</label><br>
+          <input @click="handleCatFilter('Événement')" type="checkbox" id="evenement" checked>
+          <label for="evenement"> Événement</label>
         </div>
       </div>
-    </div>
-    <div class="liste-prestataires" id="sponsors">
-      <h2>Sponsors</h2>
-      <div class="prestataires-row" v-for="row in getSponsorsRows()" :key="row[0].id_sponsor">
-        <div v-for="sponsor in row" :key="sponsor.id_sponsor" class="prestataire-card">
-          <CartePrestatairePerso
-            :nom="sponsor.nom_sponsor"
-            :description-accueil="sponsor.description_accueil"
-            :image="sponsor.image"
-            :pers-page-route="`/sponsor/${sponsor.id_sponsor}`"></CartePrestatairePerso>
+      <div class="listes">
+        <div class="liste-prestataires" id="prestataires" v-if="filtreType === 'prestataires'">
+          <h2>Prestataires</h2>
+          <div class="prestataires-row" v-for="row in getPrestaRows()" :key="row[0].id">
+            <div v-for="prestataire in row" :key="prestataire.id" class="prestataire-card">
+              <CartePrestatairePerso :nom="prestataire.nom" :descriptionAccueil="prestataire.description_accueil"
+                :image="prestataire.image" :pers-page-route="`/prestataire/${prestataire.id}`"></CartePrestatairePerso>
+            </div>
+          </div>
+        </div>
+        <div class="liste-prestataires" id="sponsors" v-if="filtreType === 'sponsors'">
+          <h2>Sponsors</h2>
+          <div class="prestataires-row" v-for="row in getSponsorsRows()" :key="row[0].id_sponsor">
+            <div v-for="sponsor in row" :key="sponsor.id_sponsor" class="prestataire-card">
+              <CartePrestatairePerso :nom="sponsor.nom_sponsor" :description-accueil="sponsor.description_accueil"
+                :image="sponsor.image" :pers-page-route="`/sponsor/${sponsor.id_sponsor}`"></CartePrestatairePerso>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -37,12 +57,16 @@ import BanniereAccueil from "@/components/BanniereAccueil.vue";
 import CartePrestatairePerso from "@/components/CartePrestatairePerso.vue";
 import { mapState, mapActions } from "vuex";
 import TotalDons from "@/components/totalDons.vue";
+import { filter } from "core-js/internals/array-iteration";
 
 export default {
   name: "PagePrincipale",
   data() {
     return {
       prestatairesRows: [],
+      filtreSearch: "",
+      filtreCategory: ['Activité', 'Mascotte', 'Gastronomie', 'Événement'],
+      filtreType: "prestataires",
     };
   },
   computed: {
@@ -50,20 +74,21 @@ export default {
     ...mapState('sponsors', ['sponsors']),
   },
   methods: {
+    filter,
     ...mapActions('prestataire', ['getAllPrestataires']), // on récupère la méthode de récupération des prestataires du store
     ...mapActions('sponsors', ['getAllSponsors']),
     getPrestaRows() {
       this.getAllPrestataires(); // charge les prestataires depuis les données
-      let prestataires = this.prestataires;
+      let prestataires = this.getFilteredPrestataires(this.prestataires);
       let rows = [];
       for (let i = 0; i < prestataires.length; i += 4) { // Ajusté pour 4 blocs par ligne
         rows.push(prestataires.slice(i, i + 4));
       }
       return rows;
     },
-    getSponsorsRows(){
+    getSponsorsRows() {
       this.getAllSponsors(); // charge les prestataires depuis les données
-      let sponsors = this.sponsors;
+      let sponsors = this.getFilteredSponsors(this.sponsors);
       let rows = [];
       for (let i = 0; i < sponsors.length; i += 4) { // Ajusté pour 4 blocs par ligne
         rows.push(sponsors.slice(i, i + 4));
@@ -74,6 +99,24 @@ export default {
     handleCommentSent(data) {
       console.log(data);
       return data;
+    },
+    handleCatFilter(cat) {
+      if (this.filtreCategory.includes(cat)) this.filtreCategory.splice(this.filtreCategory.findIndex((f) => f == cat), 1);
+      else this.filtreCategory.push(cat);
+    },
+    getFilteredPrestataires(prestataires) {
+      let catFilteredPrestataires = [];
+      for (const cat of this.filtreCategory) {
+        for (const prest of prestataires) {
+          if (!catFilteredPrestataires.includes(prest) && prest.categorie === cat)
+            catFilteredPrestataires.push(prest)
+        }
+      }
+      console.log(catFilteredPrestataires);
+      return prestataires.filter((p) => p.nom.includes(this.filtreSearch) && catFilteredPrestataires.includes(p));
+    },
+    getFilteredSponsors(sponsors) {
+      return sponsors.filter(s => s.nom_sponsor.includes(this.filtreSearch));
     },
   },
   components: {
@@ -86,6 +129,14 @@ export default {
 
 
 <style scoped>
+.listes-container{
+  display: flex;
+  flex-direction: row;
+}
+.listes{
+  display: flex;
+  flex-direction: column;
+}
 .liste-prestataires {
   display: flex;
   flex-direction: column;
@@ -98,8 +149,8 @@ export default {
 
 .prestataires-row {
   display: flex;
-  flex-wrap: wrap; 
-  gap: 20px; 
+  flex-wrap: wrap;
+  gap: 20px;
   justify-content: center;
   margin-bottom: 20px;
 }
@@ -108,10 +159,9 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: calc(25% - 20px); /* Ajusté pour quatre blocs par ligne avec espace */
+  width: calc(25% - 20px);
+  /* Ajusté pour quatre blocs par ligne avec espace */
   box-sizing: border-box;
   margin-bottom: 20px;
 }
-
 </style>
-
